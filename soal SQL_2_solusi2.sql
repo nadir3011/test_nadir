6a8d8f5f5aa73f7fc60102c0463fe3fb4fe00e1f
Nadir <nadir.musketeerz@gmail.com>
2021-10-13 00:04:40 +0800

    Add files via upload

---
+++ soal SQL_2_solusi2.sql
@@ -0,0 +1,9 @@
+SELECT nil.nik, sis.nama, pel.nama_pelajaran, (nil.nilai) AS nial
+FROM nilai nil
+JOIN pelajaran pel
+ON pel.id_pelajaran = nil.id_pelajaran
+JOIN siswa sis
+ON nil.nik = sis.nik
+WHERE nil.id_nilai=(SELECT nilX.id_nilai FROM nilai nilX WHERE nilX.nik=nil.nik ORDER BY nilX.nilai DESC LIMIT 1)
+ORDER BY nil.nik
+;
